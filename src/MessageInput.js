89067b94efeebe26aab1faaa05758f16b1d79919
Patainea Robert <patainearobert@yahoo.com>
2022-02-18 16:19:55 +0200

    fixed server

--- src/MessageInput.js
+++ src/MessageInput.js
@@ -1,25 +1,42 @@
-import React, { useState } from 'react';
-import './MessageInput.css';
+import React, { useState } from "react";
+import "./MessageInput.css";
 
-const NewMessage = ({socket}) => {
-  const [value, setValue] = useState('');
+const NewMessage = ({ socket }) => {
+  const [value, setValue] = useState("");
+  const [username, setUsername] = useState("Anonymous");
+
   const submitForm = (e) => {
     e.preventDefault();
-    socket.emit('message', value);
-    setValue('');
+    socket.emit("message", {
+      message: value,
+      username: username
+    });
+    setValue("");
   };
 
   return (
-    <form onSubmit={submitForm}>
+    <>
+      <form onSubmit={submitForm}>
+        <input
+          name="message"
+          autoFocus
+          value={value}
+          placeholder="Type your message"
+          onChange={(e) => {
+            setValue(e.currentTarget.value);
+          }}
+        />
+      </form>
       <input
-        autoFocus
-        value={value}
-        placeholder="Type your message"
+        name="username"
+        style={{ marginTop: 8, width: "500px", marginLeft: 20 }}
+        value={username}
+        placeholder="Type your username"
         onChange={(e) => {
-          setValue(e.currentTarget.value);
+          setUsername(e.currentTarget.value);
         }}
       />
-    </form>
+    </>
   );
 };
 
